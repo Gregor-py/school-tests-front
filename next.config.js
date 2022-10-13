a29afc4ba7199671e1a55513487d47afbe4d6dd0
Vitaliy <vitaliydps4#gmail.com>
2022-10-13 13:44:27 +0300

    init next js app and first settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  poweredByHeader: false,
+  optimizeFonts: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+    APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4200/api/:path*'
+      },
+      {
+        source: '/uploads/:path*',
+        description: 'http://localhost4200/uploads/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
