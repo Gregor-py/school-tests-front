d7e687c66991aae8a2c6efe094ef77e6384d3b5b
Vitaliy <vitaliydps4#gmail.com>
2022-10-19 20:30:11 +0300

    customize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,98 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+const primary = '#535DB0';
+
 module.exports = {
   content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './app/components/**/*/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/components/**/*/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary,
+      },
+    },
+    keyframes: {
+      fade: {
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      },
+      scaleIn: {
+        '0%': {
+          opacity: 0,
+          transform: 'scale(0.9)',
+        },
+        '50%': {
+          opacity: 0.3,
+        },
+        '100%': {
+          opacity: 1,
+          transform: 'scale(1)',
+        },
+      },
+    },
+    animation: {
+      fade: 'fade .5s ease-in-out',
+      scaleIn: 'scaleIn .35s ease-in-out',
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(({ addComponents, theme, addUtilities }) => {
+      addComponents({
+        '.btn-primary': {
+          backgroundColor: primary,
+          color: '#fff',
+          borderRadius: '0.65rem',
+          transition: 'background-color .3s ease-in-out',
+          '&:hover': {
+            backgroundColor: '#ff0009',
+          },
+        },
+
+        '.text-link': {
+          textUnderlineOffset: 4,
+          color: 'rgba(255, 255, 255, .9)',
+          transition: 'text-decoration-color .3s ease-in-out',
+          textDecorationLine: 'underline',
+          textDecorationColor: 'rgba(255, 255, 255, 0.2)',
+          '&:hover': {
+            textDecorationColor: 'rgba(255, 255, 255, 0.9)',
+          },
+        },
+
+        '.air-block': {
+          borderRadius: theme('borderRadius.layout'),
+          backgroundColor: theme('colors.gray.950'),
+          color: theme('colors.white'),
+          boxShadow: theme('boxShadow.lg'),
+        },
+      }),
+        addUtilities({
+          '.text-shadow': {
+            textShadow: '1px 1px rgba(0, 0, 0, 0.4)',
+          },
+
+          '.outline-border-none': {
+            outline: 'none',
+            border: 'none',
+          },
+
+          '.flex-center-between': {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          },
+
+          '.image-like-bg': {
+            objectPosition: 'center',
+            objectFit: 'cover',
+            pointerEvents: 'none',
+          },
+        });
+    }),
+  ],
+};
