7d0accbc613c79e2cb4171554683689ebef46b43
Vitaliy <vitaliydps4@gmail.com>
2023-03-01 19:40:49 +0100

    create main layout for test present

--- app/components/screens/test-present/TestPresent.tsx
+++ app/components/screens/test-present/TestPresent.tsx
@@ -1,16 +1,50 @@
 import Meta from '@/utils/meta/Meta';
 import { FC } from 'react';
-import { useQuery } from 'react-query';
-import { TestService } from '@/services/test/test.service';
-import { EditTestService } from '@/services/test/edit-test.service';
-import { GetStaticProps } from 'next';
 import { ITest } from '@/shared/types/test.types';
+import styles from './TestPresent.module.scss';
+import Image from 'next/image';
+import { subjectImageData } from '@/ui/test-present/subject-image.data';
 
 const TestPresent: FC<{ test: ITest }> = ({ test }) => {
   return (
-    <Meta title={''} description={''}>
-      <div>{test.title}</div>
-      <div>{test.description}</div>
+    <Meta title={test.title} description={test.description}>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <Image
+            src={subjectImageData[test.subject.slug]}
+            alt={test.subject.name}
+            layout={'fill'}
+            objectFit={'cover'}
+          />
+        </div>
+        <div className={styles.bottom}>
+          <h1 className={styles.title}>{test.title}</h1>
+          <div className={styles.description}>{test.description}</div>
+          <div className={styles.stats}>
+            <p>
+              <div>Предмет:</div>
+              <span className="bg-primary">{test.subject.name}</span>
+            </p>
+            <p>
+              <div>Кількість завдань:</div>
+              <span className="bg-emerald-600">{test.tasks.length}</span>
+            </p>
+            <p>
+              <div>Сподобалось:</div>
+              <span className="bg-pink-700">55</span>
+            </p>
+            <p>
+              <div>Пройдено:</div>
+              <span className="bg-violet-600">316</span>
+            </p>
+            <p>
+              <div>Клас:</div>
+              <span className="bg-amber-500">{test.class}</span>
+            </p>
+          </div>
+        </div>
+        <button className={styles.startButton}>Розпочати тест</button>
+      </div>
     </Meta>
   );
 };
